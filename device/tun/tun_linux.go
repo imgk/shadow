3bec5ee1e0f6fb92d8cb6a38a223c766c8982477
John Xiong <imgk@mail.ustc.edu.cn>
2020-06-23 19:37:45 +0800

    rtentry

--- device/tun/tun_linux.go
+++ device/tun/tun_linux.go
@@ -179,23 +179,6 @@ func (d *Device) Activate(addr, mask, gateway string) error {
 	return nil
 }
 
-//https://github.com/torvalds/linux/blob/master/include/uapi/linux/route.h
-type rtEntry struct {
-	rt_pad1    uint32
-	rt_dst     unix.RawSockaddrInet4
-	rt_gateway unix.RawSockaddrInet4
-	rt_genmask unix.RawSockaddrInet4
-	rt_flags   uint16
-	rt_pad2    int16
-	rt_pad3    uint32
-	rt_pad4    uintptr
-	rt_metric  int16
-	rt_dev     uintptr
-	rt_mtu     uint32
-	rt_window  uint32
-	rt_irtt    uint16
-}
-
 func (d *Device) AddRoute(cidr []string) error {
 	return d.modifyRouteTable(cidr, unix.SIOCADDRT)
 }
