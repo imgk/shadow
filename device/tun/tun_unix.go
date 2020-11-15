4279799df5fda2fa1e64f2a28f863899c6b78099
John Xiong <imgk@mail.ustc.edu.cn>
2020-11-15 21:45:45 +0800

    tun unix: add io.Reader, io.ReaderFrom

--- device/tun/tun_unix.go
+++ device/tun/tun_unix.go
@@ -24,6 +24,7 @@ type Device struct {
 		Mask    [16]byte
 		Gateway [16]byte
 	}
+	buf  []byte
 	buff []byte
 }
 
@@ -40,8 +41,25 @@ func CreateTUN(name string, mtu int) (dev *Device, err error) {
 	if dev.MTU, err = dev.NativeTun.MTU(); err != nil {
 		return
 	}
+	dev.buf = make([]byte, 4+dev.MTU)
 	dev.buff = make([]byte, 4+dev.MTU)
 	return
+}
+
+func (d *Device) Read(b []byte) (int, error) {
+	n, err := d.NativeTun.Read(d.buf, 4)
+	if err != nil {
+		return 0, err
+	}
+	if len(b) < n - 4 {
+		return 0, io.ErrShortBuffer
+	}
+	n = copy(b, d.buf[4:n])
+	return n, err
+}
+
+func (d *Device) ReadOffset(b []byte, offset int) (int, error) {
+	return d.NativeTun.Read(b, offset)
 }
 
 func (d *Device) WriteTo(w io.Writer) (n int64, err error) {
@@ -78,4 +96,39 @@ func (d *Device) WriteTo(w io.Writer) (n int64, err error) {
 func (d *Device) Write(b []byte) (int, error) {
 	n := copy(d.buff[4:], b)
 	return d.NativeTun.Write(d.buff[:4+n], 4)
+}
+
+func (d *Device) WriteOffset(b []byte, offset int) (int, error) {
+	return d.NativeTun.Write(b, offset)
+}
+
+func (d *Device) ReadFrom(r io.Reader) (n int64, err error) {
+	b := d.buff
+	for {
+		nr, er := r.Read(b[4:])
+		if nr > 0 {
+			nw, ew := d.NativeTun.Write(b[:4+n], 4)
+			if nw > 0 {
+				n += int64(nw)
+			}
+			if ew != nil {
+				err = ew
+				break
+			}
+			if nr != nw {
+				err = io.ErrShortWrite
+				break
+			}
+		}
+		if er != nil {
+			if errors.Is(er, os.ErrClosed) {
+				break
+			}
+			if er != io.EOF {
+				err = er
+			}
+			break
+		}
+	}
+	return
 }
