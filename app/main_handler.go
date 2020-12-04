f562a2128639f35a85efa896b5a5b05359ce6391
John Xiong <imgk@mail.ustc.edu.cn>
2020-12-04 19:57:42 +0800

    main.handler: show speed and data

--- app/main_handler.go
+++ app/main_handler.go
@@ -6,18 +6,190 @@ import (
 	"math/rand"
 	"net"
 	"net/http"
+	"sort"
+	"strconv"
 	"sync"
+	"sync/atomic"
+	"time"
 
 	"github.com/imgk/shadow/common"
 )
 
-type Conn struct {
+// netConn is methods shared by net.Conn and common.PacketConn
+type netConn interface {
 	io.Closer
-	Network    string
-	LocalAddr  net.Addr
-	RemoteAddr net.Addr
+	LocalAddr() net.Addr
+	RemoteAddr() net.Addr
+	SetDeadline(time.Time) error
+	SetReadDeadline(time.Time) error
+	SetWriteDeadline(time.Time) error
 }
 
+// Reader implements net.Conn.Read and common.PacketConn.ReadTo
+// and record the number of bytes
+type Reader struct {
+	num     uint64
+	conn    net.Conn
+	pktConn common.PacketConn
+}
+
+func (r *Reader) Read(b []byte) (n int, err error) {
+	n, err = r.conn.Read(b)
+	atomic.AddUint64(&r.num, uint64(n))
+	return
+}
+
+func (r *Reader) Close() error {
+	if closer, ok := r.conn.(common.CloseReader); ok {
+		return closer.CloseRead()
+	}
+	return r.conn.Close()
+}
+
+func (r *Reader) ReadTo(b []byte) (n int, addr net.Addr, err error) {
+	n, addr, err = r.pktConn.ReadTo(b)
+	atomic.AddUint64(&r.num, uint64(n))
+	return
+}
+
+func (r *Reader) ByteNum() uint64 {
+	return atomic.LoadUint64(&r.num)
+}
+
+// Writer implements net.Conn.Write and common.PacketConn.WriteFrom
+// and record the number of bytes
+type Writer struct {
+	num     uint64
+	conn    net.Conn
+	pktConn common.PacketConn
+}
+
+func (w *Writer) Write(b []byte) (n int, err error) {
+	n, err = w.conn.Write(b)
+	atomic.AddUint64(&w.num, uint64(n))
+	return
+}
+
+func (w *Writer) Close() error {
+	if closer, ok := w.conn.(common.CloseWriter); ok {
+		return closer.CloseWrite()
+	}
+	return w.conn.Close()
+}
+
+func (w *Writer) WriteFrom(b []byte, addr net.Addr) (n int, err error) {
+	n, err = w.pktConn.WriteFrom(b, addr)
+	atomic.AddUint64(&w.num, uint64(n))
+	return
+}
+
+func (w *Writer) ByteNum() uint64 {
+	return atomic.LoadUint64(&w.num)
+}
+
+// Conn implements net.Conn and common.PacketConn
+// and record the number of bytes it reads and writes
+type Conn struct {
+	netConn
+	Reader        Reader
+	Writer        Writer
+	preTime       time.Time
+	preRead       uint64
+	preWrite      uint64
+	Network       string
+	LocalAddress  net.Addr
+	RemoteAddress net.Addr
+}
+
+func NewConnFromConn(conn net.Conn, addr net.Addr) (c *Conn) {
+	c = &Conn{
+		netConn: conn,
+		Reader: Reader{
+			num:     0,
+			conn:    conn,
+			pktConn: nil,
+		},
+		Writer: Writer{
+			num:     0,
+			conn:    conn,
+			pktConn: nil,
+		},
+		preTime:       time.Now(),
+		preRead:       0,
+		preWrite:      0,
+		Network:       "TCP",
+		LocalAddress:  conn.RemoteAddr(),
+		RemoteAddress: addr,
+	}
+	return
+}
+
+func NewConnFromPacketConn(conn common.PacketConn) (c *Conn) {
+	c = &Conn{
+		netConn: conn,
+		Reader: Reader{
+			num:     0,
+			conn:    nil,
+			pktConn: conn,
+		},
+		Writer: Writer{
+			num:     0,
+			conn:    nil,
+			pktConn: conn,
+		},
+		preTime:       time.Now(),
+		preRead:       0,
+		preWrite:      0,
+		Network:       "UDP",
+		LocalAddress:  conn.RemoteAddr(),
+		RemoteAddress: conn.LocalAddr(),
+	}
+	return
+}
+
+func (c *Conn) Read(b []byte) (int, error) {
+	return c.Reader.Read(b)
+}
+
+func (c *Conn) CloseRead() error {
+	return c.Reader.Close()
+}
+
+func (c *Conn) Write(b []byte) (int, error) {
+	return c.Writer.Write(b)
+}
+
+func (c *Conn) CloseWrite() error {
+	return c.Writer.Close()
+}
+
+func (c *Conn) ReadTo(b []byte) (int, net.Addr, error) {
+	return c.Reader.ReadTo(b)
+}
+
+func (c *Conn) WriteFrom(b []byte, addr net.Addr) (int, error) {
+	return c.Writer.WriteFrom(b, addr)
+}
+
+func (c *Conn) Nums() (rb uint64, rs uint64, wb uint64, ws uint64) {
+	rb = c.Reader.ByteNum()
+	wb = c.Writer.ByteNum()
+
+	prev := c.preTime
+	c.preTime = time.Now()
+	duration := c.preTime.Sub(prev).Seconds()
+
+	rs = uint64(float64(rb-c.preRead) / duration)
+	ws = uint64(float64(wb-c.preWrite) / duration)
+
+	c.preRead = rb
+	c.preWrite = wb
+
+	return
+}
+
+// Handler implements common.Handler which can record all active
+// connections
 type Handler struct {
 	common.Handler
 
@@ -35,9 +207,10 @@ func NewHandler(h common.Handler) *Handler {
 
 func (h *Handler) Handle(conn net.Conn, addr net.Addr) (err error) {
 	key := rand.Uint32()
+	conn = NewConnFromConn(conn, addr)
 
 	h.mu.Lock()
-	h.conns[key] = &Conn{Closer: conn, Network: "TCP", LocalAddr: conn.RemoteAddr(), RemoteAddr: addr}
+	h.conns[key] = conn.(*Conn)
 	h.mu.Unlock()
 
 	err = h.Handler.Handle(conn, addr)
@@ -51,9 +224,10 @@ func (h *Handler) Handle(conn net.Conn, addr net.Addr) (err error) {
 
 func (h *Handler) HandlePacket(conn common.PacketConn) (err error) {
 	key := rand.Uint32()
+	conn = NewConnFromPacketConn(conn)
 
 	h.mu.Lock()
-	h.conns[key] = &Conn{Closer: conn, Network: "UDP", LocalAddr: conn.RemoteAddr(), RemoteAddr: conn.LocalAddr()}
+	h.conns[key] = conn.(*Conn)
 	h.mu.Unlock()
 
 	err = h.Handler.HandlePacket(conn)
@@ -65,29 +239,75 @@ func (h *Handler) HandlePacket(conn common.PacketConn) (err error) {
 	return
 }
 
-func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	type Item struct {
-		Protocol    string `json:"protocol"`
-		Source      string `json:"source_address"`
-		Destination string `json:"destination_address`
+func (h *Handler) Close() (err error) {
+	h.mu.Lock()
+	for _, c := range h.conns {
+		c.Close()
+	}
+	h.mu.Unlock()
+	err = h.Handler.Close()
+	return
+}
+
+type ByteNum uint64
+
+func (n ByteNum) String() (str string) {
+	const mask = (^uint64(0)) >> (64 - 10)
+
+	str = ""
+	for _, unit := range []string{" B", " K, ", " M, ", " G, ", " T, "} {
+		if n > 0 {
+			str = strconv.FormatUint(uint64(n)&mask, 10) + unit + str
+			n = n >> 10
+			continue
+		}
+		if str == "" {
+			str = "0 B"
+		}
 	}
 
-	type Conns struct {
-		Items []Item `json:"connections"`
+	return
+}
+
+func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	type ConnItem struct {
+		ConnID        uint32   `json:"id"`
+		Protocol      string   `json:"protocol"`
+		Source        net.Addr `json:"source_address"`
+		Destination   net.Addr `json:"destination_address`
+		Upload        ByteNum  `json:"upload_bytes"`
+		UploadSpeed   ByteNum  `json:"upload_speed"`
+		Download      ByteNum  `json:"download_bytes"`
+		DownloadSpeed ByteNum  `json:"download_speed"`
 	}
 
 	h.mu.RLock()
-	conns := Conns{Items: make([]Item, 0, len(h.conns))}
-	for _, c := range h.conns {
-		conns.Items = append(conns.Items, Item{
-			Protocol:    c.Network,
-			Source:      c.LocalAddr.String(),
-			Destination: c.RemoteAddr.String(),
+	conns := make([]*ConnItem, 0, len(h.conns))
+	for k, c := range h.conns {
+		rb, rs, wb, ws := c.Nums()
+		conns = append(conns, &ConnItem{
+			ConnID:        k,
+			Protocol:      c.Network,
+			Source:        c.LocalAddress,
+			Destination:   c.RemoteAddress,
+			Upload:        ByteNum(rb),
+			UploadSpeed:   ByteNum(rs),
+			Download:      ByteNum(wb),
+			DownloadSpeed: ByteNum(ws),
 		})
 	}
 	h.mu.RUnlock()
 
-	connsTemplate.Execute(w, conns)
+	sort.Slice(conns, func(i, j int) bool {
+		return conns[i].ConnID < conns[j].ConnID
+	})
+
+	type ConnsInfo struct {
+		ConnNum   int
+		ConnSlice []*ConnItem
+	}
+
+	connsTemplate.Execute(w, ConnsInfo{ConnNum: len(conns), ConnSlice: conns})
 }
 
 var connsTemplate = template.Must(template.New("").Parse(`
@@ -114,19 +334,29 @@ tr:nth-child(even) {
 </head>
 <body>
 
-<h2>Active Connections</h2>
+<h2>Active Connections - {{ .ConnNum }}</h2>
 
 <table>
   <tr>
+    <th>ID</th>
     <th>Protocol</th>
     <th>Source Address</th>
     <th>Destination Address</th>
+    <th>Upload Bytes</th>
+    <th>Upload Speed</th>
+    <th>Download Bytes</th>
+    <th>Download Speed</th>
   </tr>
-  {{ range .Items }}
+  {{ range .ConnSlice }}
   <tr>
+    <td>{{ .ConnID }}</td>
     <td>{{ .Protocol }}</td>
     <td>{{ .Source }}</td>
     <td>{{ .Destination }}</td>
+    <td>{{ .Upload }}</td>
+    <td>{{ .UploadSpeed }}</td>
+    <td>{{ .Download }}</td>
+    <td>{{ .DownloadSpeed }}</td>
   </tr>
   {{ end }}
 </table>
--- go.mod
+++ go.mod
@@ -11,11 +11,11 @@ require (
 	github.com/xtaci/smux v1.5.15
 	go.uber.org/multierr v1.6.0
 	go.uber.org/zap v1.16.0
-	golang.org/x/crypto v0.0.0-20201124201722-c8d3bf9c5392
-	golang.org/x/net v0.0.0-20201110031124-69a78807bb2b
-	golang.org/x/sys v0.0.0-20201130072748-111129e158e2
+	golang.org/x/crypto v0.0.0-20201203163018-be400aefbc4c
+	golang.org/x/net v0.0.0-20201202161906-c7110b5ffcbb
+	golang.org/x/sys v0.0.0-20201202213521-69691e467435
 	golang.org/x/time v0.0.0-20200630173020-3af7569d3a1e
 	golang.zx2c4.com/wireguard v0.0.20201119-0.20201127121345-b6303091fc8c
-	golang.zx2c4.com/wireguard/windows v0.3.2
-	gvisor.dev/gvisor v0.0.0-20201127205929-1913cff58732
+	golang.zx2c4.com/wireguard/windows v0.3.3
+	gvisor.dev/gvisor v0.0.0-20201204040109-0ba39926c86f
 )
